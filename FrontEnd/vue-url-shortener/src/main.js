a741661271e37b7e54f156138e45a6cdddf3ad28
CJBiohacker <cj.moyses@gmail.com>
2024-01-29 00:39:31 -0300

    feat: add vueitfy package and configuration

--- FrontEnd/vue-url-shortener/src/main.js
+++ FrontEnd/vue-url-shortener/src/main.js
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import './style.css';
+import 'vuetify/styles';
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(vuetify).mount('#app');

909813137be325f9425577cf770827ecdde6a69a
CJBiohacker <cj.moyses@gmail.com>
2024-02-02 17:11:50 -0300

    feat: add the Header Component

---
+++ FrontEnd/vue-url-shortener/src/components/Header/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+      <h1>
+        {{ title }}
+      </h1>
+    </header>
+</template>
+
+<script src="./Header.js">
+</script>
+
+<style scoped lang="scss">
+header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #9c5dfe;
+    height: 10dvh;
+
+    h1 {
+        color: white;
+    }
+}
+</style>

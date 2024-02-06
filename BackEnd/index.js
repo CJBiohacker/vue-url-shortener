6754e7fa6702493454b192c4759fc82e5cc8be59
CJBiohacker <cj.moyses@gmail.com>
2024-02-06 20:00:34 -0300

    fix: solve CORS bug when requesting data from server

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
@@ -12,113 +12,117 @@ const utils = require("./src/common/utils");
 const urlService = require("./src/services/url.service");
 
 const corsOptions = {
-    origin: 'http://localhost:8080',
-    optionsSuccessStatus: 200
+  origin: `${process.env.CLIENT_URL}`,
+  optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.listen(port, () => console.log(`Listening in port: ${port}`));
 
 app.get("/all", async (req, res) => {
-    let resp;
+  let resp;
 
-    try {
-        const urls = await urlService.findAll();
-        const formattedURls = [];
+  try {
+    const urls = await urlService.findAll();
+    const formattedURls = [];
 
-        for (const item of urls) {
-            formattedURls.push({
-                shortUrlId: item.shortUrlId,
-                longURL: item.longURL,
-            });
-        }
-        resp = res.status(200).send(formattedURls);
-
-        return resp;
-    } catch (error) {
-        resp = res.status(500).send("<h3>Error. Please try again.</h3>");
-
-        return resp;
+    for (const item of urls) {
+      formattedURls.push({
+        shortUrlId: item.shortUrlId,
+        longURL: item.longURL,
+      });
     }
+    resp = res.status(200).send(formattedURls);
+
+    return resp;
+  } catch (error) {
+    resp = res.status(500).send("<h3>Error. Please try again.</h3>");
+
+    return resp;
+  }
 });
 
 app.get("/:shortUrlId", async (req, res) => {
-    let resp;
+  let resp;
 
-    try {
-        const url = await urlService.find(req.params.shortUrlId);
-        resp = !url ? res.status(404).send("<h3>ShortURL ID not found.</h3>") : res.redirect(301, url.longURL);
+  try {
+    const url = await urlService.find(req.params.shortUrlId);
+    resp = !url
+      ? res.status(404).send("<h3>ShortURL ID not found.</h3>")
+      : res.redirect(301, url.longURL);
 
-        return resp;
-    } catch (error) {
-        resp = res.status(500).send("<h3>Error. Please try again.</h3>");
+    return resp;
+  } catch (error) {
+    resp = res.status(500).send("<h3>Error. Please try again.</h3>");
 
-        return resp;
-    }
+    return resp;
+  }
 });
 
 app.post("/url", async (req, res) => {
-    let resp;
+  let resp;
 
-    try {
-        if (!!utils.validateUrl(req.body.url)) {
-            return res.status(400).send(`<h3>Invalid URL.</h3>`);
-        }
-
-        const urlKey = utils.generateUrlKey();
-        const shortUrl = `http://${host}:${port}/${urlKey}`;
-
-        await urlService.save(req.body.url, shortUrl, urlKey);
-        resp = res.status(200).send({ shortUrl });
-
-        return resp;
-    } catch (error) {
-        resp = res.status(500).send(`<h3>The following error has occurred: ${error}.</h3>`);
-
-        return resp;
+  try {
+    if (!!utils.validateUrl(req.body.url)) {
+      return res.status(400).send(`<h3>Invalid URL.</h3>`);
     }
+
+    const urlKey = utils.generateUrlKey();
+    const shortUrl = `http://${host}:${port}/${urlKey}`;
+    await urlService.save(req.body.url, shortUrl, urlKey);
+    resp = res.status(200).send({ shortUrl });
+
+    return resp;
+  } catch (error) {
+    resp = res
+      .status(500)
+      .send(`<h3>The following error has occurred: ${error}.</h3>`);
+
+    return resp;
+  }
 });
 
 app.delete("/:shortUrlId", async (req, res) => {
-    let resp;
-    try {
-        const url = await urlService.find(req.params.shortUrlId);
-        resp = !url ? res.status(404).send("<h3>ShortURL ID not found.</h3>") : await urlService.erase(req.params.shortUrlId);
+  let resp;
+  try {
+    const url = await urlService.find(req.params.shortUrlId);
+    resp = !url
+      ? res.status(404).send("<h3>ShortURL ID not found.</h3>")
+      : await urlService.erase(req.params.shortUrlId);
 
-        return resp;
-    } catch (error) {
-        resp = res.status(500).send("<h3>Error. Please try again.</h3>");
+    return resp;
+  } catch (error) {
+    resp = res.status(500).send("<h3>Error. Please try again.</h3>");
 
-        return resp;
-    }
+    return resp;
+  }
 });
 
 app.put("/:shortUrlId", async (req, res) => {
-    let resp, longURL;
+  let resp, longURL;
 
-    try {
-        if (req.body.longURL) {
-            longURL = req.body.longURL;
-        } else {
-            resp = await urlService.find(req.params.shortUrlId);
-            longURL = resp.longURL;
-        }
-
-        const shortUrlId = { shortUrlId: req.params.shortUrlId };
-        const update = await urlService.update(shortUrlId, longURL);
-        if (update.matchedCount === 0) {
-            resp = res.status(404).send(`<h3>URL was not found.</h3>`);
-        } else {
-            resp = res.status(200).send(`<h3>URL updated.</h3>`);
-        }
-
-        return resp;
-    } catch (error) {
-        resp = res.status(500).send("<h3>Error. Please try again.</h3>");
-
-        return resp;
+  try {
+    if (req.body.longURL) {
+      longURL = req.body.longURL;
+    } else {
+      resp = await urlService.find(req.params.shortUrlId);
+      longURL = resp.longURL;
     }
 
+    const shortUrlId = { shortUrlId: req.params.shortUrlId };
+    const update = await urlService.update(shortUrlId, longURL);
+    if (update.matchedCount === 0) {
+      resp = res.status(404).send(`<h3>URL was not found.</h3>`);
+    } else {
+      resp = res.status(200).send(`<h3>URL updated.</h3>`);
+    }
+
+    return resp;
+  } catch (error) {
+    resp = res.status(500).send("<h3>Error. Please try again.</h3>");
+
+    return resp;
+  }
 });
--- FrontEnd/vue-url-shortener/vite.config.js
+++ FrontEnd/vue-url-shortener/vite.config.js
@@ -1,52 +1,53 @@
-import Components from 'unplugin-vue-components/vite'
-import Vue from '@vitejs/plugin-vue'
-import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-import ViteFonts from 'unplugin-fonts/vite'
+import Components from "unplugin-vue-components/vite";
+import Vue from "@vitejs/plugin-vue";
+import Vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+import ViteFonts from "unplugin-fonts/vite";
 
 // Utilities
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     Vue({
-      template: { transformAssetUrls }
+      template: { transformAssetUrls },
     }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
     Vuetify({
       autoImport: true,
       styles: {
-        configFile: 'src/styles/style.css',
+        configFile: "src/styles/style.css",
       },
     }),
     Components(),
     ViteFonts({
       google: {
-        families: [{
-          name: 'Roboto',
-          styles: 'wght@100;300;400;500;700;900',
-        }],
+        families: [
+          {
+            name: "Roboto",
+            styles: "wght@100;300;400;500;700;900",
+          },
+        ],
       },
     }),
   ],
-  define: { 'process.env': {} },
+  define: { "process.env": {} },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ],
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
   server: {
     port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:6969",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
-})
-
+});
